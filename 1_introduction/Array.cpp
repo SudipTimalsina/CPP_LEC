42ee47058f4a6e3acc1d3bc612d6560eaa0015da
SudipTimalsina <timalsinasudip11@gmail.com>
2022-05-19 14:08:59 +0545

    function overloading/ default argument

---
+++ 1_introduction/Array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5]={4,3,6,2,8};
+    cout << "The Numbers are: ";
+
+    for (int i = 0; i < 5; ++i) {
+    cout << arr[i] << "  ";
+    }
+    return 0;
+}
---
+++ chapter_3_intro/functions/default_arg.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sum(int x,int y,int z)
+{
+    return (x+y+z);
+}
+int sum(int a=3,int b=5)
+{
+    return (a+b);
+}
+int main()
+{
+    int added = sum(5,3,4);
+    sum();
+    cout <<" The sum is: "<< sum(5,5);
+
+}
+
